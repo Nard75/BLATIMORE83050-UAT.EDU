4acf1a03e771acd8754832e0d5dfc1ccc3914639
Nard75 <40498495+Nard75@users.noreply.github.com>
2024-05-28 18:09:15 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace ASSIGNMENT2._1GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+    }
+}
